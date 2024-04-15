836f82b500093c3d35911828efe13248f1091470
chiefdey <deynohmacharia@gmail.com>
2024-04-15 10:22:58 +0300

    update_three

---
+++ binding.js
@@ -0,0 +1,8 @@
+//getting js data to html
+let f_name="denis"
+let s_name="chief"
+let full_name=f_name+" "+s_name
+document.getElementById("first").innerHTML=full_name
+//getting html to js
+let form = document.getElementById("myform")
+form.addEventListener("submit")
